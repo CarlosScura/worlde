406a745ee12c9d7ccfc520809c236627fdfe7f0d
Federico Alarcón Scura <carlos.a.scura@gmail.com>
2026-01-28 14:25:06 -0300

    Se agregó la funcion fun_intetnos() para controlar la cantidad de intentos que tiene el usuario

--- wordle.py
+++ wordle.py
@@ -1,4 +1,6 @@
-# VARIABLES
+'''
+    VARIABLES
+'''
 
 # variable para la cantidad de intentos dentro del juego
 intentos = 6
@@ -13,3 +15,21 @@ palabra_ingresada = ''
 numero = 5
 
 ganaste = False
+
+'''
+    FUNCIONES
+'''
+
+# definir la cantidad de intentos = variable
+def fun_intentos():
+    global palabra_ingresada
+    global numero
+    global intentos
+    if intentos > 0:
+        print(f"tienes {intentos} intentos")
+        intentos -= 1
+        palabra_ingresada = input("Ingrese una palabra")
+        print(f"la palabra ingresada es: {palabra_ingresada}")
+        tamano_palabra(palabra_ingresada,numero)
+    else:
+        print("Perdiste :(\nMejor suerte la proxima")
