8e4277ad3e70b1b93bc7c369a20b2eadfa245230
Federico Alarcón Scura <carlos.a.scura@gmail.com>
2026-01-28 14:38:47 -0300

    Se agregó la función verificador_palabra() que controla que las letras esten en la palabra secreta

--- wordle.py
+++ wordle.py
@@ -56,3 +56,25 @@ def transformar(palabra):
         palabras_ingresadas.append(palabra_convertida)
         return palabras_ingresadas
 
+# Controlar palabras
+
+def verificador_palabra(palabra_ingresada, palabra_secreta):
+    global numero
+    global ganaste
+    for i in range(numero):
+        las_palabras_son_iguales = palabra_ingresada[i] == palabra_secreta[i]   # True or False
+        la_letra_existe_en_la_palabra = palabra_ingresada[i] in palabra_secreta # True or False
+        if las_palabras_son_iguales:
+            letras_verificadas.append(f"[{palabra_ingresada[i]}]")
+            ganaste = True # si esto pasa ultimo se gana igual
+        elif la_letra_existe_en_la_palabra:
+            letras_verificadas.append(f"({palabra_ingresada[i]})")
+            ganaste = False
+        else:
+            letras_verificadas.append(palabra_ingresada[i])
+            ganaste = False
+    if not ganaste:
+        imprimir_matriz(letras_verificadas)
+    else:
+        ganador(palabra_ingresada)
+
