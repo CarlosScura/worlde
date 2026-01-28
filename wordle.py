d0a1b25ede4e969a933ffcf91716ae492b414e56
Federico Alarcón Scura <carlos.a.scura@gmail.com>
2026-01-28 14:20:04 -0300

    Se agregan las primeras variables para nuestro wordle

---
+++ wordle.py
@@ -0,0 +1,15 @@
+# VARIABLES
+
+# variable para la cantidad de intentos dentro del juego
+intentos = 6
+
+letras_verificadas = []
+cantidad_letras = 5
+
+palabra_secreta = "gatos"
+
+palabra_ingresada = ''
+
+numero = 5
+
+ganaste = False
